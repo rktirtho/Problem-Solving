8fa9d893d1914fcb31619dccbd8f1a4c0d3f37c9
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-07-26 16:05:34 +0600

    Sequence IJ 1

---
+++ src/problem/solving/SequenceIJ1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problem.solving;
+
+/**
+ *
+ * @author rktirtho
+ */
+public class SequenceIJ1 {
+    public static void main(String[] args) {
+        
+        for (int i=1,j = 60; j >=0; j-=5, i+=3) {
+            System.out.println("I="+i+" J="+j);
+        }
+                
+                
+    }
+}

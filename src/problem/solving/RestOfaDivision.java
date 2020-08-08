483b13278275629948646fd6df01479cda959036
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-08-08 23:44:32 +0600

    1133 - Rest of a Division code simplify

--- src/problem/solving/RestOfaDivision.java
+++ src/problem/solving/RestOfaDivision.java
@@ -12,25 +12,21 @@ import java.util.Scanner;
  * @author rktirtho
  */
 public class RestOfaDivision {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        if (x < y) {
-            for (int i = x+1; i < y; i++) {
-                if (i % 5 == 3 || i % 5 == 2) {
-                    System.out.println(i);
-                }
-            }
+        int x= sc.nextInt();
+        int y= sc.nextInt();
+        if (x>y) {
+            int temp =x;
+            x=y;
+            y=temp;
         }
-        if (x>y){
-            for (int i = y+1; i < x; i++) {
-                if (i % 5 == 3 || i % 5 == 2) {
-                    System.out.println(i);
-                }
+        
+        for (int i = x+1; i < y; i++) {
+            if (i%5==3 || i%5==2) {
+                System.out.println(i);
             }
         }
     }
-
+    
 }

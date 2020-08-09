0f669c47b571136f92c62e753add3c0910aaaae5
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-08-10 01:05:58 +0600

    remove bad doumentation in game time

--- src/problem/solving/GameTime.java
+++ src/problem/solving/GameTime.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package problem.solving;
 
 import java.util.Scanner;
 
-/**
- *
- * @author DELL
- */
+
 public class GameTime {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);

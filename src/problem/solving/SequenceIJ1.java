980ba0ceea0bb488bba9fa9c453c32c050263ee8
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-08-10 01:03:04 +0600

    remove bad comment

--- src/problem/solving/SequenceIJ1.java
+++ src/problem/solving/SequenceIJ1.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package problem.solving;
 
-/**
- *
- * @author rktirtho
- */
+
 public class SequenceIJ1 {
     public static void main(String[] args) {
         
